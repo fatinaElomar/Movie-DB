eb1ea0f370b7ee708735545c29f6220b95010a57
fatinaElomar <fatina-el-omar@hotmail.co.uk>
2022-12-26 23:21:53 +0200

    step 4 completed

--- index.js
+++ index.js
@@ -16,7 +16,23 @@ app.get("/time", (req, res) => {
   res.send({ status: 200, message: time });
 });
 
+app.get("/hello/:ID", (req, res) => {
+  data = req.params;
+  res.send({ status: 200, message: "Hello, " + data.ID });
+});
+
+app.get("/search", (req, res) => {
+  const search = req.query.s;
+  if (typeof search == "undefined" || search === "") {
+    res.send({
+      status: 500,
+      error: true,
+      message: "you have to provide a search",
+    });
+  } else {
+    res.send({ status: 200, message: "ok", data: search });
+  }
+});
 app.listen(path, () => {
   console.log(`the app listening on path at http://localhost:${path}`);
 });
-//add commit 
