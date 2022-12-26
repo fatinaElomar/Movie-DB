039501da51517de5fe6c92c81c6b85c9ea2290f9
fatinaElomar <fatina-el-omar@hotmail.co.uk>
2022-12-26 20:40:07 +0200

    step 3 completed

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const path = 3000
+const express = require("express");
+const app = express();
+const path = 3000;
 
-app.get('/', (req, res) => {
-  res.send('okay')
-})
+app.get("/", (req, res) => {
+  res.send("okay");
+});
+
+app.get("/test", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+
+app.get("/time", (req, res) => {
+  const currenttime = new Date();
+  const time = currenttime.getHours() + ":" + currenttime.getSeconds();
+  res.send({ status: 200, message: time });
+});
 
 app.listen(path, () => {
-  console.log(`Example app listening on port at http://localhost:${path}`)
-})
+  console.log(`the app listening on path at http://localhost:${path}`);
+});
+//add commit 
